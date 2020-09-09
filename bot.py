9be55c5ec28b04212d37c864336a8e8098c5e75e
riccardo berto <ricki1993.scorpione@gmail.com>
2020-09-09 08:22:49 +0200

    Fixed Deleted Command

--- bot.py
+++ bot.py
@@ -12,8 +12,10 @@ bot = commands.Bot(command_prefix='$')
 blacklist = ['verorip', 'On4me']
 
 @commands.command()
-async def punisci(ctx, member:discord.Member, *args):
-    if(len[i for i in range(len(blacklist)) if blacklist[i] in member.display_name]>0):
+async def p(ctx, member:discord.Member, *args):
+
+    await ctx.message.channel.purge(limit=1)
+    if(any([blacklist[i] in member.display_name for i in range(len(blacklist))])):
         print('nope')
         return
 
@@ -21,7 +23,6 @@ async def punisci(ctx, member:discord.Member, *args):
     channel = discord.utils.get(guild.voice_channels, name='PunishChanel')
     if channel==None: return
     await member.edit(voice_channel=channel)
-    await bot.delete_message(ctx.message)
     vc = await channel.connect()
 
     vc.play(discord.FFmpegPCMAudio(executable="D:/Python/DiscordShoutBot/ffmpeg-20200831-4a11a6f-win64-static/bin/ffmpeg.exe", source="testing.mp3"), after=lambda e: print('done', e))
@@ -36,5 +37,5 @@ async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
 
-bot.add_command(punisci)
+bot.add_command(p)
 bot.run('token')
